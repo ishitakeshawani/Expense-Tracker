8657068fecd7331dfe05f899179f4ebd9e0da8da
Brijraj <brijrajparmar27@gmail.com>
2021-10-01 17:35:55 +0530

    minor formatting

--- scripts/sandbox.js
+++ scripts/sandbox.js
@@ -1,100 +1,99 @@
+const headingEl = document.querySelector("#headingTotal");
 
-        const headingEl = document.querySelector("#headingTotal");
-
-        const inputDescEl = document.querySelector("#inputDesc");
-        const inputElement = document.querySelector("#inputAmount");
-        const expenseTableEl = document.querySelector("#expanseTable");
-
-
-        let totalExpense = 0;
-
-        headingEl.textContent = totalExpense;
-
-        const allExpenses = [];
+const inputDescEl = document.querySelector("#inputDesc");
+const inputElement = document.querySelector("#inputAmount");
+const expenseTableEl = document.querySelector("#expanseTable");
 
 
-        function allExpenseToTotal(){
-            
-            const expenseItem = {};
+let totalExpense = 0;
 
-            const textAmount = inputElement.value;
-            const textDesc = inputDescEl.value;
-            
-            const expense = parseInt(textAmount,10);
+headingEl.textContent = totalExpense;
 
-            expenseItem.desc = textDesc;
-            expenseItem.amount = expense;
-            expenseItem.moment = new Date(); 
-           
-
-            allExpenses.push(expenseItem);
-            
-            totalExpense = totalExpense + expense; 
-            
-            //const someText = "the expense for this month" + totalExpense;
-
-            const someText  = `Total : ${totalExpense}`
-
-            headingEl.textContent = someText;
+const allExpenses = [];
 
 
-            const allExpenseHTML = allExpenses.map(expense =>  createListItem(expense));
-            const joinAllExpenseHTML = allExpenseHTML.join("");
-            
-            
-            expenseTableEl.innerHTML = joinAllExpenseHTML;
-        }
+function allExpenseToTotal(){
+    
+    const expenseItem = {};
+
+    const textAmount = inputElement.value;
+    const textDesc = inputDescEl.value;
+    
+    const expense = parseInt(textAmount,10);
+
+    expenseItem.desc = textDesc;
+    expenseItem.amount = expense;
+    expenseItem.moment = new Date(); 
+   
+
+    allExpenses.push(expenseItem);
+    
+    totalExpense = totalExpense + expense; 
+    
+    //const someText = "the expense for this month" + totalExpense;
+
+    const someText  = `Total : ${totalExpense}`
+
+    headingEl.textContent = someText;
+
+
+    const allExpenseHTML = allExpenses.map(expense =>  createListItem(expense));
+    const joinAllExpenseHTML = allExpenseHTML.join("");
+    
+    
+    expenseTableEl.innerHTML = joinAllExpenseHTML;
+}
 
        
-        const element = document.querySelector("#btnAddExpense");
-        
-        element.addEventListener("click",allExpenseToTotal,false);
+const element = document.querySelector("#btnAddExpense");
 
-        function getDateString(moment){
-           return moment.toLocaleDateString('en-US',{year: 'numeric',
-                                    month:'long',
-                                    day:'numeric'})
-        }
+element.addEventListener("click",allExpenseToTotal,false);
 
-        function deleteItem(dateValue){
+function getDateString(moment){
+   return moment.toLocaleDateString('en-US',{year: 'numeric',
+    month:'long',
+    day:'numeric'})
+}
 
-            const newArr = allExpenses.filter(expense => expense.moment.valueOf() !== dateValue)
-            renderList(newArr);   
-        // allExpenses.forEach(item => {
-        //     if(item.moment.valueOf==dateValue){
-        //         let pos = allExpenses.indexOf(item);
-        //         allExpenses.splice(pos,1);
-        //         totalExpense-=item.amount;
-        //     }
-        // });
+function deleteItem(dateValue){
+
+    const newArr = allExpenses.filter(expense => expense.moment.valueOf() !== dateValue)
+    renderList(newArr);   
+// allExpenses.forEach(item => {
+//     if(item.moment.valueOf==dateValue){
+// let pos = allExpenses.indexOf(item);
+// allExpenses.splice(pos,1);
+// totalExpense-=item.amount;
+//     }
+// });
        // renderList(allExpenses);
-        }
+}
 
-        function renderList(arrOfList){
-            const allExpenseHTML = arrOfList.map(expense =>  createListItem(expense));
-            const joinAllExpenseHTML = allExpenseHTML.join("");
-            expenseTableEl.innerHTML = joinAllExpenseHTML;
-            
-        }
+function renderList(arrOfList){
+    const allExpenseHTML = arrOfList.map(expense =>  createListItem(expense));
+    const joinAllExpenseHTML = allExpenseHTML.join("");
+    expenseTableEl.innerHTML = joinAllExpenseHTML;
+    
+}
 
        function createListItem({desc,amount,moment}){
-           return `<li class="list-group-item d-flex justify-content-between">
+   return `<li class="list-group-item d-flex justify-content-between">
 							<div class="d-flex flex-column">
-                                ${desc}
-                                <small class="text-muted">
-                                    ${getDateString(moment)}
-                                    </small>
+${desc}
+<small class="text-muted">
+    ${getDateString(moment)}
+    </small>
 							</div>
 							<div>
 								<span class="px-5">
 									${amount}
 								</span>
-                                <button type="button" class="btn btn-outline-danger btn-sm"
-                                onclick="deleteItem(${moment.valueOf()})"
-                                >
+<button type="button" class="btn btn-outline-danger btn-sm"
+onclick="deleteItem(${moment.valueOf()})"
+>
 									<i class="fas fa-trash-alt"></i>
 								</button>
 							</div>
 						</li>
-                        `
+`
        }
